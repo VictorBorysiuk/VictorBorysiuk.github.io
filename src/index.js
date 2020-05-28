81409af923dcf269d7e7b18489c00f67332faaac
Viktor <borysyukviktor@gmail.com>
2020-05-28 21:03:22 +0300

    change adding PIXI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import PIXI from 'pixi.js';
+import * as PIXI from 'pixi.js';
+/* import (* as projection from) 'pixi-projection';
+import _slicedToArray from '@babel/runtime/helpers/esm/slicedToArray';
+import _objectWithoutProperties from '@babel/runtime/helpers/esm/objectWithoutProperties' */
 
 const width = 800;
 const height = 450;
