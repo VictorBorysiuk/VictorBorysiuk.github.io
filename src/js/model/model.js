2274d114bab143a562cb2ad302d579de8920373a
Viktor <borysyukviktor@gmail.com>
2020-06-01 12:51:17 +0300

    fix border widht in canvas

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -2,8 +2,8 @@ export default class Model {
    constructor() {
    let app;
    let canvasContainer = document.getElementById("showPixi");
-   let width = canvasContainer.innerWidth || 726;
-   let height = canvasContainer.innerHeight || width/2;
+   let width = canvasContainer.innerWidth || 800 ;
+   let height = canvasContainer.innerHeight || 450;
    const colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba]; //массив цветов
    let gravity = 4;
    let figuresAmount = 0; //количество созданных фигур
